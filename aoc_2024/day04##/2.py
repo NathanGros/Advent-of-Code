f3313827078ce407eb1f0b8c94f921359fd2d8da
NathanGros <nathan.gros6@gmail.com>
2024-12-04 07:05:59 +0100

    added comments to day 4 of 2024

--- aoc_2024/day04##/2.py
+++ aoc_2024/day04##/2.py
@@ -1,9 +1,11 @@
-lines = [l.split()[0] for l in open("input.txt")]
+lines = [l[:-1] for l in open("input.txt")]
 
 res=0
 for i in range(len(lines[0]) - 2):
     for j in range(len(lines) - 2):
+        #make a string which contains the caracters of the cross starting at i j and going down and right
         s = lines[j][i] + lines[j][i+2] + lines[j+1][i+1] + lines[j+2][i] + lines[j+2][i+2]
+        #check for the 4 possible orientations of the searched cross
         if s == 'MMASS' or s == 'SMASM' or s == 'SSAMM' or s == 'MSAMS':
             res += 1
 
