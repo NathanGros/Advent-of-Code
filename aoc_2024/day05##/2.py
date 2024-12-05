ea543093169d43a88d1f2759a041e1f0144ce67c
NathanGros <nathan.gros6@gmail.com>
2024-12-05 13:49:53 +0100

    upgraded and commented code of day 5 of 2024

--- aoc_2024/day05##/2.py
+++ aoc_2024/day05##/2.py
@@ -1,13 +1,12 @@
 lines = [l[:-1] for l in open("input.txt")]
 
 endRules = 0
-for l in lines:
-    if l == '':
-        endRules += 1
-        break
-    endRules += 1
+for i in range(len(lines)):
+    if lines[i] == '':
+        endRules = i+1
 
 def isPrintable(update):
+    #check if all rules correctly applied
     for j in range(0, endRules-1):
         rule = lines[j].split('|')
         for k in range(len(update)-1):
@@ -16,46 +15,35 @@ def isPrintable(update):
                     return False
     return True
 
-def insertSort(rules, l):
+def insertSort(rules, update):
     sortedUpdate = []
-    for e in l:
+    #for every element , insert it at a position where no rules are broken
+    for e in update:
         insertIndex = 0
-        canInsert = False
-        while (not canInsert) and insertIndex < len(l):
-            flag = True
+        #iterate through sortedUpdate to find insertIndex
+        while insertIndex < len(sortedUpdate):
+            canInsert = True
+            #check if no rules are broken
             for r in rules:
-                if r[0] == e and r[1] in sortedUpdate:
-                    if sortedUpdate.index(r[1]) < insertIndex:
-                        flag = False
-                elif r[1] == e and r[0] in sortedUpdate:
-                    if insertIndex <= sortedUpdate.index(r[0]):
-                        flag = False
-            canInsert = flag
-            if not canInsert:
+                if ((r[0] == e and r[1] in sortedUpdate and sortedUpdate.index(r[1]) < insertIndex)
+                or (r[1] == e and r[0] in sortedUpdate and insertIndex <= sortedUpdate.index(r[0]))):
+                    canInsert = False
+            if canInsert:
+                break
+            else:
                 insertIndex += 1
         sortedUpdate.insert(insertIndex, e)
     return sortedUpdate
 
 
 
-updates = []
-for i in range(endRules, len(lines)):
-    updates.append(lines[i].split(','))
-
-unprintables = []
-for i in range(len(lines) - endRules):
-    update = updates[i]
-    printable = isPrintable(update)
-    if not printable:
-        if update not in unprintables:
-            unprintables.append(update)
-
-rules = []
-for i in range(0, endRules-1):
-    rules.append(lines[i].split('|'))
+updates = [l.split(',') for l in lines[endRules:]]
+unprintables = [u for u in updates if not isPrintable(u)]
+rules = [l.split('|') for l in lines[:endRules-1]]
 
 res = 0
 for update in unprintables:
     sortedUpdate = insertSort(rules, update)
+    #get middle number
     res += int(sortedUpdate[int((len(sortedUpdate)+1)/2)-1])
 print(res)
