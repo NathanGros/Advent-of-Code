c4d2ced03cd5245e505b0aca5578638e001afed3
NathanGros <nathan.gros6@gmail.com>
2024-12-05 13:49:53 +0100

    upgraded and commented code of day 5 of 2024

--- aoc_2024/day05##/1.py
+++ aoc_2024/day05##/1.py
@@ -1,26 +1,26 @@
 lines = [l[:-1] for l in open("input.txt")]
 
 endRules = 0
-for l in lines:
-    if l == '':
-        endRules += 1
-        break
-    endRules += 1
+for i in range(len(lines)):
+    if lines[i] == '':
+        endRules = i+1
 
-updates = []
-for i in range(endRules, len(lines)):
-    updates.append(lines[i].split(','))
-
-res = 0
-for i in range(len(lines) - endRules):
-    update = updates[i]
-    printable = True
+def isPrintable(update):
+    #check if all rules correctly applied
     for j in range(0, endRules-1):
         rule = lines[j].split('|')
         for k in range(len(update)-1):
             for l in range(k+1, len(update)):
                 if rule[1] == update[k] and rule[0] == update[l]:
-                    printable = False
-    if printable:
+                    return False
+    return True
+
+updates = [l.split(',') for l in lines[endRules:]]
+
+res = 0
+for i in range(len(lines) - endRules):
+    update = updates[i]
+    if isPrintable(update):
+        #get middle number
         res += int(update[int((len(update)+1)/2)-1])
 print(res)
