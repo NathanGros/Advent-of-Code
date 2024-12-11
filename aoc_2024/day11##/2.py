af5bdc41cdb4dcf106e7dc836e30ffd24cc06526
NathanGros <nathan.gros6@gmail.com>
2024-12-11 19:33:00 +0100

    improved code of day 11 part 2 of 2024

--- aoc_2024/day11##/2.py
+++ aoc_2024/day11##/2.py
@@ -1,39 +1,30 @@
 line = [list(map(int, l.split())) for l in open("input.txt")][0]
 
-#dictionary of number of descendants of a value after n blinks
-vals = {}
+#dictionary of number of descendants of a stone after n blinks
+stones = {}
 
-def findNb(val, n):
-    #if no blink then return 1
+def findnbchildren(stone, n):
+    #if no blink then no children so return 1
     if n == 0:
         return 1
+    #return the stoneue if it is already know
+    if (stone, n) in stones:
+        return stones[stone, n]
+    #if the stoneue is not in the dictionary then compute it
+    if stone == 0:
+        res = findnbchildren(1, n-1)
+    elif len(str(stone)) % 2 == 1:
+        res = findnbchildren(stone * 2024, n-1)
     else:
-        #return the value if it is already know
-        if (val, n) in vals:
-            return vals[val, n]
-        #if the value is not in the dictionary then compute it and add it to the dictionary
-        else:
-            l = []
-            #problem rules
-            if val == 0:
-                l.append(1)
-            elif len(str(val)) % 2 == 1:
-                l.append(val * 2024)
-            else:
-                s = str(val)
-                halfLength = int(len(s)/2)
-                l.append(int(s[:halfLength]))
-                l.append(int(s[halfLength:]))
-            res = 0
-            #compute descendants
-            for v in l:
-                key = findNb(v, n-1)
-                vals[v, n-1] = key
-                res += key
-            return res
+        s = str(stone)
+        halflength = int(len(s)/2)
+        res = findnbchildren(int(s[:halflength]), n-1) + findnbchildren(int(s[halflength:]), n-1)
+    #add the stone to the dictionary
+    stones[stone, n] = res
+    return res
 
 res = 0
 for n in line:
     #compute for every stone of the input, for 75 blinks
-    res += findNb(n, 75)
+    res += findnbchildren(n, 75)
 print(res)
