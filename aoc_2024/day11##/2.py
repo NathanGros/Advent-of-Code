33c34818f34fdfb3361e90516b67108f6327e897
NathanGros <nathan.gros6@gmail.com>
2024-12-11 19:34:55 +0100

    changed function case of day 11 part 2 of 2024

--- aoc_2024/day11##/2.py
+++ aoc_2024/day11##/2.py
@@ -3,7 +3,7 @@ line = [list(map(int, l.split())) for l in open("input.txt")][0]
 #dictionary of number of descendants of a stone after n blinks
 stones = {}
 
-def findnbchildren(stone, n):
+def findNbChildren(stone, n):
     #if no blink then no children so return 1
     if n == 0:
         return 1
@@ -12,13 +12,13 @@ def findnbchildren(stone, n):
         return stones[stone, n]
     #if the stoneue is not in the dictionary then compute it
     if stone == 0:
-        res = findnbchildren(1, n-1)
+        res = findNbChildren(1, n-1)
     elif len(str(stone)) % 2 == 1:
-        res = findnbchildren(stone * 2024, n-1)
+        res = findNbChildren(stone * 2024, n-1)
     else:
         s = str(stone)
         halflength = int(len(s)/2)
-        res = findnbchildren(int(s[:halflength]), n-1) + findnbchildren(int(s[halflength:]), n-1)
+        res = findNbChildren(int(s[:halflength]), n-1) + findNbChildren(int(s[halflength:]), n-1)
     #add the stone to the dictionary
     stones[stone, n] = res
     return res
@@ -26,5 +26,5 @@ def findnbchildren(stone, n):
 res = 0
 for n in line:
     #compute for every stone of the input, for 75 blinks
-    res += findnbchildren(n, 75)
+    res += findNbChildren(n, 75)
 print(res)
