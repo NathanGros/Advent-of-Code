941774ba2d8cb297ddd1d019edbbe87e94f5891e
NathanGros <nathan.gros6@gmail.com>
2024-12-13 06:49:21 +0100

    solved day 13 of 2024

---
+++ aoc_2024/day13##/2.py
@@ -0,0 +1,19 @@
+lines = [l.split() for l in open("input.txt")]
+groups = []
+for i in range(int((len(lines)+1)/4)):
+    groups.append([lines[4*i], lines[4*i+1], lines[4*i+2]])
+
+data = [[int(a[2][2:-1]), int(a[3][2:]), int(b[2][2:-1]), int(b[3][2:]), int(p[1][2:-1]), int(p[2][2:])] for [a, b, p] in groups]
+
+res = 0
+for [ax, ay, bx, by, x, y] in data:
+    x += 10000000000000
+    y += 10000000000000
+    i = (bx * y - by * x) / (ay * bx - ax * by)
+    j = (x - i * ax) / bx
+    isInt = False
+    if (x - i * ax) % bx == 0 and (bx * y - by * x) % (ay * bx - ax * by) == 0:
+        isInt = True
+    if isInt:
+        res += 3 * int(i) + int(j)
+print(res)
