710e0c5164cf84d95e5a8a9e6872037f23c3dfef
NathanGros <nathan.gros6@gmail.com>
2025-12-02 06:42:27 +0100

    solved day 2 of 2025

--- aoc_2025/day02__/2.py
+++ aoc_2025/day02__/2.py
@@ -1,1 +1,25 @@
-lines = [l for l in open("input.txt")]
+lines = [l[:-1].split(',') for l in open("input.txt")][0]
+lines = [l.split('-') for l in lines]
+
+def isIdValid(s):
+    for n in range(1, len(s) // 2 + 1):
+        # only get divisors of len(s)
+        if len(s) % n != 0:
+            continue
+        checkDigit = 0
+        flagInvalidId = True
+        while (checkDigit < len(s)):
+            # print(s[checkDigit:checkDigit+n])
+            if s[checkDigit:checkDigit+n] != s[:n]:
+                flagInvalidId = False
+            checkDigit += n
+        if flagInvalidId:
+            return False
+    return True
+
+res = 0
+for l in lines:
+    for id in range(int(l[0]), int(l[1]) + 1):
+        if not isIdValid(str(id)):
+            res += id
+print(res)
